8139697505e27bb4747ea8610b8ae1266859e71d
Tadiwa Pfachi <pfachitadiwanashedavid@gmail.com>
2025-06-05 10:06:08 +0200

    update mason and config to stop checking for .mappings.server that is not exposed api anymore acording to chatgpt

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,1 +1,15 @@
-return { "mason-org/mason-lspconfig.nvim", config = function() end }
+return {
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = { "lua_ls", "rust_analyzer" },
+		handlers = {
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
+		},
+	},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
+}
